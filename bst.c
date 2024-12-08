5e1e37c04bb27fc65829d99e7d7f2e4c5c0a2332
Bella <2770706h@student.gla.ac.uk>
2024-12-08 21:44:04 +0000

    added parent pointer back to rewritten methods

--- bst.c
+++ bst.c
@@ -15,11 +15,10 @@ void destroyTree(node_t * node);
 void insert(node_t * node, int elem);
 void delete(node_t * node, int elem);
 
-node_t* insert_rec(node_t* node, int elem); 
+node_t* insert_rec(node_t* node, int elem, node_t** ptr_to_node); 
 
 //implementation
 node_t * search(node_t * node, int toFind) {
-	printf("in node %d\n", node->value);
 	if (node == NULL || node->value == toFind) {
 		return node;
 	} if (toFind > node->value) {
@@ -41,18 +40,13 @@ void destroyTree(node_t * node) {
 }
 
 void delete(node_t * node, int elem) {
-	printf("deleting %d\n", elem);
-
 	node_t* to_delete = search(node, elem);
 	if (!to_delete) { return; }
-
-	printf("found node\n");
 
 	// if node is leaf, remove pointer in parent and free
 	if (!to_delete->left && !to_delete->right) {
 		*to_delete->ptr_to_this = NULL;
 		free(to_delete);
-		printf("deleted leaf\n");
 	}
 
 	// if node has two children, swap value with max in subtree then delete where old max in subtree is
@@ -90,27 +84,27 @@ void delete(node_t * node, int elem) {
 }
 
 void insert(node_t * node, int elem) {
-	insert_rec(node, elem);
+	insert_rec(node, elem, &node);
 }
 
-node_t* insert_rec(node_t* node, int elem) {
-	printf("inserting node %d\n", node->value);
+node_t* insert_rec(node_t* node, int elem, node_t** ptr_to_node) {
 	if (node == NULL) {
 		node_t* new = (node_t*) malloc(sizeof(node_t));
 		new->value = elem;
 		new->left = NULL;
 		new->right = NULL;
+		new->ptr_to_this = ptr_to_node;
+
 		return new;
 	} else if (elem < node->value) {
-		node->left = insert_rec(node->left, elem);
+		node->left = insert_rec(node->left, elem, &node->left);
 	} else if (elem > node->value) {
-		node->right = insert_rec(node->right, elem);
+		node->right = insert_rec(node->right, elem, &node->right);
 	}
 	return node;
 }
 
 node_t * createTree(int firstElem) {
-	printf("creating tree with first elem %d\n", firstElem);
 	node_t * node = (node_t*) malloc(sizeof(node_t));
 	node->value = firstElem;
 	node->left = NULL;
@@ -166,7 +160,6 @@ bool test_search_node_exists() {
 bool test_delete_leaf() {
 	node_t * root = createTree(100);
 	insert(root, 50);
-	printf("deleting in test\n");
 	delete(root, 50);
 
 	bool valid = true;
@@ -197,7 +190,6 @@ bool test_delete_one_child() {
 
 	bool valid = true;
 	if (root->left->value != 500 && root->left->left->value != 125) { valid = false; }
-	printf("root: %d, left: %d, leftleft: %d\n", root->value, root->left->value, root->left->left->value);
 	return valid;
 }
 
@@ -221,7 +213,6 @@ bool run_test(bool (*script)(), char name[]) {
 void test_suite() {
 	run_test(&test_insert_create, "test_insert_create()");
 	run_test(&test_destroy_tree, "test_destroy_tree()");
-	printf("about to run test search node exists\n");
 	run_test(&test_search_node_exists, "test_search_node_exists()");
 	run_test(&test_delete_leaf, "test_delete_leaf()");
 	run_test(&test_delete_two_children, "test_delete_two_children()");
